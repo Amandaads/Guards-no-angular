c494cf9bb6520412bcf44535cf8c88601c396009
Amanda <amanda.profissional078@gmail.com>
2025-03-30 05:36:42 -0300

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogingComponent } from "./components/login/loging.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
