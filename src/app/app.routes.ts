7a09193c7b9077629cdf0d4e6e85a86afdeb2eca
Amanda <amanda.profissional078@gmail.com>
2025-03-30 05:39:59 -0300

    importando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { LogingComponent } from './components/login/loging.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+        pathMatch:'full'
+    },
+    {  
+        path:'sobre',
+        component: SobreComponent,
+        pathMatch:'full'
+        
+    },
+    {
+        path:'produtos',
+        component: ProdutosComponent,
+        pathMatch: 'full'
+    }, 
+    {
+        path:'login', 
+        component: LogingComponent, 
+        
+    }
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
